eceb4fbd29cdc362aa7789ea03dd92f437425457
unknown <ejazzarafshan@gmail.com>
2024-07-27 01:56:23 +0400

    hehe

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function menuClick(){
+    document.getElementsByClassName('smallNavMenu')[0].classList.toggle('smallNavMenuDeactivate')
+    
+
+    document.getElementsByClassName('fa-times')[0].classList.toggle('iconX')
+    document.getElementsByClassName('fa-bars')[0].classList.toggle('iconBar')
+}
+
